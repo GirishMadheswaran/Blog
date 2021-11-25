b8444280969e5126f093eeacdd8ee22fbddc18e1
GirishMadheswaran <girishgv997@gmail.com>
2021-11-25 17:43:48 +0530

    initial commit

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+ function Header() {
+    return (
+        <div className='header'>
+            <div className="headerTitles">
+                <span className='headerTitleSmall'>Blog</span>
+            </div>
+            {/* <img className="headerImg" src="./images/2.jpg" alt="" /> */}
+        </div>
+    )
+}
+
+export default Header;
